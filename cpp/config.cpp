955109db216b3fdd9bb4235ac77fd536a4665b52
exersalza <squidodle44@gmail.com>
2023-04-05 14:22:04 +0200

    still dont know what i did i'm such a bad dev

--- cpp/config.cpp
+++ cpp/config.cpp
@@ -1,9 +1,9 @@
 #include "config.hpp"
 
 #include <filesystem>
-#include <iostream>
 #include <fstream>
-#include <string>
+#include <sstream>
+#include <limits>
 
 namespace fs = std::filesystem;
 
@@ -11,6 +11,16 @@ struct parsed_line {
     std::string key;
     std::string value;
 };
+
+template <typename T>
+bool is_numeric(const std::string& str) {
+    std::istringstream iss(str);
+    T num;
+
+    if (!(iss >> num) || (!iss.eof() && iss.peek() != '\n')) return false;
+
+    return (num >= std::numeric_limits<T>::min() && num <= std::numeric_limits<T>::max());
+}
 
 std::vector<std::string> split(const std::string& str, const std::string& del) {
     std::regex reg(del);
@@ -21,6 +31,9 @@ std::vector<std::string> split(const std::string& str, const std::string& del) {
     return ret;
 }
 
+std::string strip(const std::string& str) {
+    return std::regex_replace(str, std::regex("^\\s+|\\s+$"), "");
+}
 
 Config::Config(std::string &&path) : path(path) {
     find_config();
@@ -39,7 +52,6 @@ int Config::find_config() {
         auto file_name = file.path().filename();
 
         if (std::regex_match(file_name.c_str(), f_bare)) {
-
             config_path = file_name.string();
             return 0;
         }
@@ -62,12 +74,18 @@ int Config::load_config() {
 
     while (std::getline(file, line)) {
         // sort out empty and comment lines.
-        if (line.empty() || line.length() < 2 || (line[0] == '/' && line[1] == ' ')) {
+        if (line.empty() || line.length() < 2) {
             last_category = "";
             continue;
         }
 
-        if (line[0] == '/' && line[1] == '/') continue;
+        if (std::regex_match(line, std::regex("^//.+|^/.+"))) {
+            std::cout << "yeet\n";
+            continue;
+        }
+
+
+        line = std::regex_replace(line, std::regex("//.+$"), "");
 
         // get category and lists
         if (std::regex_match(line, l_category)) {
@@ -75,6 +93,10 @@ int Config::load_config() {
             line.replace(line.find(')'), 1, "");
 
             last_category = line;
+
+            if (!config.count(last_category)) {
+                config[last_category] = std::map<std::string, std::any>();
+            }
             continue;
         }
 
@@ -82,7 +104,7 @@ int Config::load_config() {
         std::vector<std::string> split_line = split(line, "=");
 
         if (split_line.size() < 2) continue; // I don't know how you can fuck up the config.
-        parsed_line kv {split_line[0], split_line[1]};
+        parsed_line kv {strip(split_line[0]), strip(split_line[1])};
           
         if (std::regex_match(kv.value, l_is_string)) {
             kv.value = std::regex_replace(kv.value, std::regex("[\"']"), "");
@@ -90,10 +112,29 @@ int Config::load_config() {
 
         if (std::regex_match(kv.value, l_list)) {
             kv.value = std::regex_replace(kv.value, std::regex("[\\[\\]]"), "");
-            std::vector<std::string> t = split(kv.value, ", ");
+            std::vector<std::string> t = split(kv.value, ",");
+            std::vector<std::any> ret;
+            ret.reserve(t.size());
 
             for (std::string& i : t) {
-                std::cout << i << '\n';
+                std::string yep = strip(i);
+
+                // maybe not the optimal way, but it works, maybe I fix it later.
+                if (is_numeric<int>(yep)) {
+                    ret.push_back(std::stoi(yep));
+                    continue;
+                }
+                if (is_numeric<double>(yep)) {
+                    ret.push_back(std::stod(yep));
+                    continue;
+                }
+
+                ret.push_back(yep);
+            }
+
+            if (!last_category.empty()) {
+
+                config[last_category][kv.key] = ret;
             }
         }
     }
